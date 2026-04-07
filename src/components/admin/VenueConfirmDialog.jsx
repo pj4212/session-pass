3986c267e9aa3cf7937bf323f3d53a00738a7a55
PJ <pjcoor@gmail.com>
2026-04-07 05:48:28 +0000

    File changes

--- src/components/admin/VenueConfirmDialog.jsx
+++ src/components/admin/VenueConfirmDialog.jsx
@@ -78,7 +78,7 @@ export default function VenueConfirmDialog({ open, onOpenChange, event, location
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-lg">
+      <DialogContent className="max-w-lg max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle>Confirm Venue</DialogTitle>
         </DialogHeader>
